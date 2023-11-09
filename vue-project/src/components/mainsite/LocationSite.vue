9f9b82e0107b47da497a39796455dfd59425021b
이예진 <bella_floret@naver.com>
2023-11-10 03:50:35 +0900

    make location site

---
+++ vue-project/src/components/mainsite/LocationSite.vue
@@ -0,0 +1,30 @@
+<!-- <script setup>
+        // <script>
+        //   function redirectToLink(link) {
+        //     window.location.href = link; // 클릭한 카드에 설정된 링크로 이동합니다.
+        //   }
+        // </script>
+
+
+</script> -->
+<script setup>
+import LocationSiteItem from './LocationSiteItem.vue';
+</script>
+<template>
+    <section>
+        <div style="margin-top: 30px; font-size: 20px; margin-left: 13%">우리지역의</div>
+        <div id="cultural_font" style="margin-left: 13%">관광지.</div>
+        <br />
+        <div class="container">
+          <div class="row">
+            <LocationSiteItem></LocationSiteItem>
+          </div>
+        </div>
+
+      </section>
+
+</template>
+
+<style scoped>
+
+</style>
