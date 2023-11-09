484eb8664c355bda46e30f6b9635891f9525136c
asdf <asdfa@asdfas.com>
2023-11-09 16:46:02 +0900

    yejin

---
+++ vue-project/src/views/IndexView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Heading from "../components/common/Heading.vue";
+</script>
+
+<template>
+    <header>
+    <Heading></Heading>
+</header>
+    <div>
+        
+    </div>
+</template>
+
+<style scoped>
+
+</style>
