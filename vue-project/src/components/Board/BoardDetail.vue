65c6750b53a8427eb43783c9f59aea6e079285bb
user <hjlove001@gmail.com>
2023-11-18 22:39:59 +0900

    haeji

--- vue-project/src/components/Board/BoardDetail.vue
+++ vue-project/src/components/Board/BoardDetail.vue
@@ -1,71 +1,33 @@
 <script setup>
-import axios from "axios";
 import { ref, onMounted } from "vue";
-import { useRouter, useRoute } from "vue-router";
-// import BoardFormItem from "./item/BoardFormItem.vue";
 
-const route = useRoute();
-const router = useRouter();
-// const props = defineProps(["article"]);
-// const props = defineProps({ article: Object });
+const articleno = 1;
 
-const url = import.meta.env.VITE_BOARD_VIEW_API_URL;
-// const durl = import.meta.env.VITE_BOARD_DELETE_API_URL;
+const article = ref({
+  articleNo: articleno,
+  subject: "안녕하세요 뷰테스트 중입니다.",
+  content:
+    "뷰테스트 중입니다. 컴포넌트 연습하고 있으며, 앞으로 props와 custom event를 처리 할 예정입니다.",
+  userId: "ssafy",
+  userName: "김싸피",
+  hit: 123,
+  registerDate: "25.01.01",
+});
 
 onMounted(() => {
   getArticle();
-  // console.log("article:" + route.params.subject);
 });
 
 const getArticle = () => {
-  console.log(route.params.articleno + "번글 얻으러 가자!!!");
+  console.log(articleno + "번글 얻으러 가자!!!");
 };
 
-const articles = ref([]);
+function moveList() {}
 
-onMounted(() => {
-  getArticles();
-});
-
-const getArticles = () => {
-  axios
-    .get(url, {
-      params: {
-        articleNo: route.params.articleno,
-      },
-    })
-    .then(({ data }) => {
-      console.log(data.resdata);
-      articles.value = data.resdata;
-      console.log(articles.value);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
-
-function moveList() {
-  router.push({ name: "article-list" });
-}
-
-function moveModify() {
-  router.push({
-    name: "article-modify",
-  });
-}
+function moveModify() {}
 
 function onDeleteArticle() {
-  console.log(route.params.articleno + "번글 삭제하러 가자!!!");
-  axios
-    .delete(`http://localhost:8080/spring/resboard/delete/${route.params.articleno}`)
-    .then((response) => {
-      console.log(`Deleted post with ID ${route.params.articleno}`);
-      alert("삭제되었습니다");
-      router.push({ name: "article-list" });
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  console.log(articleno + "번글 삭제하러 가자!!!");
 }
 </script>
 
@@ -77,25 +39,55 @@ function onDeleteArticle() {
           <mark class="sky">글보기</mark>
         </h2>
       </div>
-      <div class="col-lg-10 text-center">
+      <div class="col-lg-10 text-start">
         <div class="row my-2">
-          <h2 class="text-secondary px-5">제목 : {{ articles.subject }}</h2>
-          <h2 class="text-secondary px-5">내용 : {{ articles.content }}</h2>
+          <h2 class="text-secondary px-5">
+            {{ article.articleNo }}. {{ article.subject }}
+          </h2>
         </div>
-      </div>
-
-      <div class="row">
-        <div class="col-md-8">
-          <div class="clearfix align-content-center"></div>
-        </div>
-
-        <div class="col-md-4">
-          <div class="d-flex justify-content-center">
-            <button type="button" class="btn btn-primary me-2" @click="moveList">글목록</button>
-
-            <button type="button" class="btn btn-success me-2" @click="moveModify">글수정</button>
-
-            <button type="button" class="btn btn-danger" @click="onDeleteArticle">글삭제</button>
+        <div class="row">
+          <div class="col-md-8">
+            <div class="clearfix align-content-center">
+              <img
+                class="avatar me-2 float-md-start bg-light p-2"
+                src="https://raw.githubusercontent.com/twbs/icons/main/icons/person-fill.svg"
+              />
+              <p>
+                <span class="fw-bold">안효인</span> <br />
+                <span class="text-secondary fw-light">
+                  {{ article.registerDate }}1 조회 : {{ article.hit }}
+                </span>
+              </p>
+            </div>
+          </div>
+          <div class="col-md-4 align-self-center text-end">댓글 : 17</div>
+          <div class="divider mb-3"></div>
+          <div class="text-secondary">
+            {{ article.content }}
+          </div>
+          <div class="divider mt-3 mb-3"></div>
+          <div class="d-flex justify-content-end">
+            <button
+              type="button"
+              class="btn btn-outline-primary mb-3"
+              @click="moveList"
+            >
+              글목록
+            </button>
+            <button
+              type="button"
+              class="btn btn-outline-success mb-3 ms-1"
+              @click="moveModify"
+            >
+              글수정
+            </button>
+            <button
+              type="button"
+              class="btn btn-outline-danger mb-3 ms-1"
+              @click="onDeleteArticle"
+            >
+              글삭제
+            </button>
           </div>
         </div>
       </div>
